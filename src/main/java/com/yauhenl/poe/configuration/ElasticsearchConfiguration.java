123b3851d8ced7d1d81df50a0c07b8ec27cbe519
yauhenl <yauhen.lisimau@gmail.com>
2017-08-27 22:21:17 +0300

    update libs

--- src/main/java/com/yauhenl/poe/configuration/ElasticsearchConfiguration.java
+++ src/main/java/com/yauhenl/poe/configuration/ElasticsearchConfiguration.java
@@ -3,7 +3,7 @@ package com.yauhenl.poe.configuration;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.transport.client.PreBuiltTransportClient;
+//import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,8 +13,8 @@ import java.net.UnknownHostException;
 @Configuration
 public class ElasticsearchConfiguration {
 
-    @Bean
-    public Client client() throws UnknownHostException {
-        return new PreBuiltTransportClient(Settings.EMPTY).addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
-    }
+//    @Bean
+//    public Client client() throws UnknownHostException {
+//        return new PreBuiltTransportClient(Settings.EMPTY).addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
+//    }
 }
